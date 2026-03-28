7784cd962c7eb92c3d4f267cae6e7713fcf3ae73
anusreesjith <anusreesjith@gmail.com>
2026-03-28 19:15:21 +0530

    feat: add sidebar

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainContainer = () => {
+  return <div className="absolute left-47">MainContainer</div>;
+};
+
+export default MainContainer;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SideBar = () => {
+  return <div className="fixed top-0 bottom-0 w-47 bg-magenta-3">SideBar</div>;
+};
+
+export default SideBar;
