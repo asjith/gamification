e0c0fcda2927d0d2dfbd9a2d80bfce86f1d2f92b
anusreesjith <anusreesjith@gmail.com>
2026-03-29 11:46:14 +0530

    feat: add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import notification from "../icons/Notification.png";
+import user from "../icons/Persona.png";
+import alert from "../icons/NotificationAlert.png";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 right-0 left-47 flex justify-between items-center h-16 px-36.5 py-4">
+      <h2 className="font-semibold text-lg text-text h-fit">Gamification</h2>
+      <div className="relative flex gap-4">
+        <div className="flex justify-center items-center w-8 h-8 cursor-pointer hover:bg-bg-surface-hover hover:rounded-10">
+          <img className="w-6 h-6" src={notification}></img>
+        </div>
+
+        <img className="absolute left-4 top-0 w-4 h-4" src={alert}></img>
+        <img className="w-8 h-8" src={user}></img>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 const MainContainer = () => {
-  return <div className="absolute left-47">MainContainer</div>;
+  return (
+    <div className="absolute left-47">
+      <Header />
+    </div>
+  );
 };
 
 export default MainContainer;
